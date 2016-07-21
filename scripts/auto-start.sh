d52fc654251b56d63182b2adf8877059298e43de
Gus Segura <kyanyoga@yahoo.com>
2016-07-20 21:36:56 -0500

    Create auto-start.sh

---
+++ scripts/auto-start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+x=5     # initialize x to 5 : dwell multiplier
+y=$1    # initialize y to $1 args1
+
+mul=$(($x * $y))   # multiply the values of x and y and assign it to variable mul
+
+
+# configure docker containers
+for ((i = 1; i <= $y; i++)); do
+    docker exec -d splunk-fwd-$i /bin/bash /opt/perfmon/bin/auto
+    sleep 5
+done
